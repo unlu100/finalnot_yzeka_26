d1a783e216c0325175b39af87941ae3ff91d98be
Ahmet Unlu <ahmetunlu25@hotmail.com>
2026-06-12 21:49:40 +0300

    Delete config2.js

--- config2.js
+++
@@ -1,3 +0,0 @@
-// config.example.js — kopyalayın -> config.js yapın (config.js commit edilmeyecek)
-window.SUPABASE_URL = 'https://dumzxgwuzzuasdlqrytw.supabase.co';
-window.SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.***';
