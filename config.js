a1f027031ef18c9da99f5a88f57db0f48509c7b8
Ahmet Unlu <ahmetunlu25@hotmail.com>
2026-06-13 00:11:21 +0300

    Add Supabase configuration to config.js
    Added configuration settings for Supabase URL and key.

---
+++ config.js
@@ -0,0 +1,3 @@
+// config.example.js — kopyalayın -> config.js yapın (config.js commit edilmeyecek)
+window.SUPABASE_URL = 'https://dumzxgwuzzuasdlqrytw.supabase.co';
+window.SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR1bXp4Z3d1enp1YXNkbHFyeXR3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzYzNTc2MzgsImV4cCI6MjA5MTkzMzYzOH0.TL3kxBsQO_mEqNpP08cg6z42i-iAdh7IEK3t3_VDZcY';
